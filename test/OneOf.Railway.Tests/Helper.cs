8797dbcac7c3570ffee3b5aacbede41e869ca8c0
Karlos348 <karoldobkowski3@gmail.com>
2024-07-05 00:26:32 +0200

    add ResultFactory tests

---
+++ test/OneOf.Railway.Tests/Helper.cs
@@ -0,0 +1,16 @@
+﻿using OneOf.Railway.Results;
+
+namespace OneOf.Railway.Tests;
+
+public static class Helper
+{
+    public static TValue GetValue<TValue>(this GlobalResult<TValue> result)
+    {
+        return result.Match(x => x.Value, _ => throw new InvalidOperationException());
+    }
+    
+    public static Failure GetFailure(this GlobalResult result)
+    {
+        return result.Match(_ => throw new InvalidOperationException(), f => f);
+    }
+}
---
+++ test/OneOf.Railway.Tests/ResultFactoryTests.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+
+namespace OneOf.Railway.Tests;
+
+public class ResultFactoryTests
+{
+    [Fact]
+    public void ShouldReturnSharedSuccessInstance()
+    {
+        var result1 = ResultFactory.Success();
+        var result2 = ResultFactory.Success();
+
+        Assert.Same(result1, result2);
+        Assert.True(result1.IsSuccess);
+        Assert.True(result2.IsSuccess);
+    }
+
+    [Fact]
+    public void ShouldReturnSuccessWithResult()
+    {
+        var expectedResult = "AEZAKMI";
+
+        var result = ResultFactory.Success(expectedResult);
+
+        Assert.True(result.IsSuccess);
+        Assert.Equal(expectedResult, result.GetValue());
+    }
+
+    [Fact]
+    public void ShouldReturnFailureWithGlobalId()
+    {
+        var globalCode = "Error123";
+
+        var result = ResultFactory.Failure(globalCode);
+
+        Assert.True(result.IsFailure);
+        Assert.Equal(globalCode,
+            result.GetFailure().GlobalCode);
+    }
+
+    [Fact]
+    public void GenericResultShouldReturnFailureWithGlobalId()
+    {
+        var globalCode = "Error456";
+
+        var result = ResultFactory.Failure<string>(globalCode);
+
+        Assert.True(result.IsFailure);
+        Assert.Equal(globalCode,
+            result.Match(_ => throw new InvalidOperationException(), failure => failure.GlobalCode));
+    }
+}
