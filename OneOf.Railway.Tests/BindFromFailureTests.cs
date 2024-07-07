68564736ffa89550b991d788a2cf0348484bd866
Karlos348 <karoldobkowski3@gmail.com>
2024-07-08 00:05:49 +0200

    change visitation checking

--- OneOf.Railway.Tests/BindFromFailureTests.cs
+++ OneOf.Railway.Tests/BindFromFailureTests.cs
@@ -8,15 +8,17 @@ public class BindFromFailureTests
     public void ToSuccess()
     {
         var error = "Error";
+        var visited = false;
         
         var result = ResultFactory.Failure(error)
             .Bind(() =>
             {
-                throw new InvalidOperationException();
+                visited = true;
                 return ResultFactory.Success();
             });
 
         Assert.True(result.IsFailure);
+        Assert.False(visited);
         Assert.Equal(error, result.GetFailure().GlobalCode);
     }
     
@@ -25,15 +27,17 @@ public class BindFromFailureTests
     {
         var value = 348;
         var error = "Error";
+        var visited = false;
         
         var result = ResultFactory.Failure(error)
             .Bind(() =>
             {
-                throw new InvalidOperationException();
+                visited = true;
                 return ResultFactory.Success(value);
             });
 
         Assert.True(result.IsFailure);
+        Assert.False(visited);
         Assert.Equal(error, result.GetFailure().GlobalCode);
     }
     
@@ -41,15 +45,17 @@ public class BindFromFailureTests
     public void ToFailure()
     {
         var error = "Error";
+        var visited = false;
         
         var result = ResultFactory.Failure(error)
             .Bind(() =>
             {
-                throw new InvalidOperationException();
+                visited = true;
                 return ResultFactory.Failure("x");
             });
 
         Assert.True(result.IsFailure);
+        Assert.False(visited);
         Assert.Equal(error, result.GetFailure().GlobalCode);
     }
     
@@ -57,15 +63,17 @@ public class BindFromFailureTests
     public void ToGenericFailure()
     {
         var error = "Error";
+        var visited = false;
         
         var result = ResultFactory.Failure(error)
             .Bind(() =>
             {
-                throw new InvalidOperationException();
+                visited = true;
                 return ResultFactory.Failure<int>("x");
             });
 
         Assert.True(result.IsFailure);
+        Assert.False(visited);
         Assert.Equal(error, result.GetFailure().GlobalCode);
     }
     
@@ -73,15 +81,17 @@ public class BindFromFailureTests
     public async Task ToAsyncSuccess()
     {
         var error = "Error";
+        var visited = false;
         
         var result = await ResultFactory.Failure(error)
             .Bind(async () =>
             {
-                throw new InvalidOperationException();
+                visited = true;
                 return await Helper.SuccessAsync();
             });
 
         Assert.True(result.IsFailure);
+        Assert.False(visited);
         Assert.Equal(error, result.GetFailure().GlobalCode);
     }
     
@@ -90,15 +100,17 @@ public class BindFromFailureTests
     {
         var error = "Error";
         var value = 348;
+        var visited = false;
         
         var result = await ResultFactory.Failure(error)
             .Bind(async () =>
             {
-                throw new InvalidOperationException();
+                visited = true;
                 return await Helper.SuccessAsync(value);
             });
 
         Assert.True(result.IsFailure);
+        Assert.False(visited);
         Assert.Equal(error, result.GetFailure().GlobalCode);
     }
 
@@ -106,15 +118,17 @@ public class BindFromFailureTests
     public async Task ToAsyncFailure()
     {
         var error = "Error";
+        var visited = false;
         
         var result = await ResultFactory.Failure(error)
             .Bind(async () =>
             {
-                throw new InvalidOperationException();
+                visited = true;
                 return await Helper.FailureAsync("another_error");
             });
 
         Assert.True(result.IsFailure);
+        Assert.False(visited);
         Assert.Equal(error, result.GetFailure().GlobalCode);
     }
     
@@ -122,15 +136,17 @@ public class BindFromFailureTests
     public async Task ToAsyncGenericFailure()
     {
         var error = "Error";
+        var visited = false;
         
         var result = await ResultFactory.Failure(error)
             .Bind(async () =>
             {
-                throw new InvalidOperationException();
+                visited = true;
                 return await Helper.FailureAsync<int>("another_error");
             });
 
         Assert.True(result.IsFailure);
+        Assert.False(visited);
         Assert.Equal(error, result.GetFailure().GlobalCode);
     }
 }
