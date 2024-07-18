fc81ea77617e65b76f095fa163a822fbe8cb7a96
Karlos348 <karoldobkowski3@gmail.com>
2024-07-18 23:49:29 +0200

    add TryGetValue tests

---
+++ OneOf.Railway.Tests/TryGetValueTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace OneOf.Railway.Tests;
+
+public class TryGetValueTests
+{
+    [Fact]
+    public void ShouldReturnTrueAndSetValue_WhenSuccess()
+    {
+        var expectedValue = 348;
+        var globalResult = ResultFactory.Success(expectedValue);
+
+        var success = globalResult.TryGetValue(out var value);
+
+        Assert.True(success);
+        Assert.Equal(expectedValue, value);
+    }
+
+    [Fact]
+    public void ShouldReturnFalseAndSetValueToDefault_WhenFailure()
+    {
+        var globalResult = ResultFactory.Failure<int>("Error");
+        
+        var success = globalResult.TryGetValue(out var value);
+        
+        Assert.False(success);
+        Assert.Equal(default, value);
+    }
+}
