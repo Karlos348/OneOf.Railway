906a864fb339a483c34e0fcf49b4856e28fc1f19
Karlos348 <karoldobkowski3@gmail.com>
2024-07-19 16:19:50 +0200

    add GetValue tests

---
+++ OneOf.Railway.Tests/GetValueTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace OneOf.Railway.Tests;
+
+public class GetValueTests
+{
+    [Fact]
+    public void ShouldReturnValue_WhenSuccess()
+    {
+        var expectedValue = 348;
+        var globalResult = ResultFactory.Success(expectedValue);
+
+        var result = OneOfGlobalResultExtensions.GetValue(globalResult);
+
+        Assert.Equal(expectedValue, result);
+    }
+
+    [Fact]
+    public void ShouldThrowInvalidOperationException_WhenFailure()
+    {
+        var globalResult = ResultFactory.Failure<int>("Error");
+        
+        Assert.Throws<InvalidOperationException>(() => OneOfGlobalResultExtensions.GetValue(globalResult));
+    }
+}
